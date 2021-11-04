b6efb1454e44e9fc7152aca213bf793e5e9376ba
StefanoCapelli <stefano.capelli3@studenti.unimi.it>
2021-11-04 21:53:39 +0100

    Initial Commit

---
+++ sistema_elettorale/src/main/java/sistema_elettorale/Elettore.java
@@ -0,0 +1,81 @@
+/**
+ * PROGETTO INGEGNERIA DEL SOFTWARE: ASSIGNMENT 2
+ * GRUPPO: CAPELLI STEFANO 922842 				LEONCINI LORENZO 930936	
+ *	 
+ */
+package sistema_elettorale;
+
+/**
+ * La classe elettore identifica un elettore tramite ID (che verrà gestito in modo univoco [es. SPID]).
+ * L'attributo aggiuntivo che si è deciso di aggiungere è il nome dell'elettore.
+ */
+public class Elettore {
+	
+	/**
+	 * Costruttore della classe 
+	 * input: ID e nome
+	 */
+	public Elettore(String iD, String nome) {
+		super();
+		
+		boolean check_result = CheckID(ID);
+		if (check_result) {
+			this.ID = iD;
+		}
+		this.nome = nome;
+	}
+
+	private String ID;
+	private String nome;
+	
+	/**
+	 * output: ID dell'elettore
+	 */
+	public String getID() {
+		return ID;
+	}
+	
+	/**
+	 * input: ID dell'elettore  
+	 */
+	public void setID(String iD) {
+		ID = iD;
+	}
+	
+	/**
+	 * output: nome dell'elettore
+	 */
+	public String getNome() {
+		return nome;
+	}
+	
+	/**
+	 * input: nome dell'elettore
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	/**
+	 * Funzione (non completa) per votare in una determinata elezione
+	 * input: scheda elettorale (ID elezione) 
+	 * output: scelta dell'elettore
+	 */
+	public void Vota( ) {
+		
+	}
+
+	/**
+	 * Funzione (non completa) per il controllo sulla validità dell'ID elettore
+	 * input: ID dell'elettore 
+	 * output: approvazione o meno dell'ID
+	 */
+	public boolean CheckID(String iD) {
+		
+		//if (true) {
+		//	return true;
+		//}
+		
+		return false;
+	}
+}
---
+++ sistema_elettorale/src/main/java/sistema_elettorale/Scrutatore.java
@@ -0,0 +1,61 @@
+/**
+ * PROGETTO INGEGNERIA DEL SOFTWARE: ASSIGNMENT 2
+ * GRUPPO: CAPELLI STEFANO 922842 				LEONCINI LORENZO 930936	
+ *	 
+ */
+package sistema_elettorale;
+
+/**
+ * La classe Scrutatore è stata implementata come idea temporanea, dato che il ruolo non era precedentemente presente.
+ * interpretandolo come entità assegnata agli scrutini (senza futura implementazione reale di questa classe)
+ *
+ */
+public class Scrutatore {
+
+	
+	private int IDscrutatore;
+	//private Elezione e; == variabile che rimanda ad una successivo oggetto Elezione non ancora presente
+
+	/**
+	 * costruttore della classe Scrutatore
+	 * input: ID dello scrutatore, Elezione
+	 */
+	public Scrutatore(int iDscrutatore) {
+		super();
+		IDscrutatore = iDscrutatore;
+	}
+		
+	/**
+	 * output: ID dello scrutatore
+	 */
+	public int getIDscrutatore() {
+		return IDscrutatore;
+	}
+	
+	/**
+	 * input: identificativo numerico dello scrutatore
+	 */
+	public void setIDscrutatore(int iDscrutatore) {
+		IDscrutatore = iDscrutatore;
+	}
+		
+	/**
+	 * Funzione (non completa) per iniziare la fase di scrutinio
+	 * input: Elezione
+	 * output: variabile booleana di successo o fallimento
+	 */
+	public void InizioScrutinio() {
+		
+	}
+	
+	
+	/**
+	 * Funzione (non completa) per ottenere i risultati dello scrutinio 
+	 * output: storico dei risultati ottenuti dello scrutatore
+	 */
+	public void GetRisultati( ) {
+		
+	}
+	
+
+}
